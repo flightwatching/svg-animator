b7cfa9be6406b4f7622db0b4f00085e91d41cb8e
kladier <kevin.ladier@gmail.com>
2017-01-20 13:45:07 +0100

    adding svg examples & putting them in the grid item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { NgGrid } from './angular2-grid/directives/NgGrid';
 import { NgGridItem } from './angular2-grid/directives/NgGridItem';
 import { NgGridConfig, NgGridItemConfig, NgGridItemEvent } from "./angular2-grid/interfaces/INgGrid";
 
-interface Box {
+class Box {
 	id: number;
 	config: NgGridItemConfig;
+	svg: string;
 }
 
 @Component({
@@ -16,7 +17,7 @@ interface Box {
 export class AppComponent {
 	private boxes: Array<Box> = [];
 
-  title = 'app works!';
+  title = 'SVG Animator';
   private gridConfig: NgGridConfig = <NgGridConfig>{
 		'margins': [5],
 		'draggable': true,
@@ -30,26 +31,35 @@ export class AppComponent {
 		'col_width': 2,
 		'row_height': 2,
 		'cascade': 'up',
-		'min_width': 50,
-		'min_height': 50,
+		'min_width': 250,
+		'min_height': 250,
 		'fix_to_grid': false,
 		'auto_style': true,
-		'auto_resize': false,
-		'maintain_ratio': false,
+		'auto_resize': true,
+		'maintain_ratio': true,
 		'prefer_new': false,
 		'zoom_on_drag': false,
 		'limit_to_screen': true
 	};
 
 	constructor() {
+		//this.boxes = new Array<Box>(4);
 		for (var i = 0; i < 4; i++) {
 			const conf = this._generateDefaultItemConfig();
 			conf.payload = 1 + i;
-			this.boxes[i] = { id: i + 1, config: conf };
+			this.boxes[i] = new Box();
+			this.boxes[i].id = i + 1;
+			this.boxes[i].config = conf;
+			//this.boxes[i] = { id: i + 1, config: conf, svg: 'assets/examples/astronaute.svg'};
 		}
+		this.boxes[0].svg = 'normalized_tectonic_plates.svg';
+		this.boxes[1].svg = 'astronaute.svg';
+		this.boxes[2].svg = 'files.svg';
+		this.boxes[3].svg = 'horloge.svg';
 	}
 
 	private _generateDefaultItemConfig(): NgGridItemConfig {
-		return { 'dragHandle': '.handle', 'col': 1, 'row': 1, 'sizex': 1, 'sizey': 1 };
+		return { 'dragHandle': '.handle', 'col': 1, 'row': 1, 'resizable': true, 'minWidth': 10, 'minHeight': 10, 'sizex': 1, 'sizey': 1 };
 	}
 }
+	
