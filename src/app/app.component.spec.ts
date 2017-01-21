9edb95a57fc4699927795293c056aa4c46dace26
kladier <kevin.ladier@gmail.com>
2017-01-21 01:38:09 +0100

    fixing tests finally

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'SVG Animator'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('SVG Animator');
