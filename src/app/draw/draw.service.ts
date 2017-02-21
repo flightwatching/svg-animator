323f86d17fa66ec0f4e49a9175e82d2e93a39cc8
NotBad4U <lightplay8@gmail.com>
2017-02-21 01:07:38 +0100

    Don't stock svg in the service anymore

--- src/app/draw/draw.service.ts
+++ src/app/draw/draw.service.ts
@@ -6,14 +6,7 @@ import {Observable} from "rxjs";
 @Injectable()
 export class DrawService {
 
-    public draws: Array<any> = [];
-
-    constructor(private http : Http) {
-        this.getDraws()
-            .subscribe(
-                data => this.draws = data.draws,
-                err => console.log(err));
-    }
+    constructor(private http : Http) {}
 
     getDraws(): Observable<any> {
         return this.http.get(`${environment.config.API_BASE_URL}draw`)
