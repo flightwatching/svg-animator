5ae833fda49f5d842df506baf462308dc1bba388
NotBad4U <lightplay8@gmail.com>
2017-02-26 14:49:35 +0100

    Add UUID to box model

--- src/app/workspace-config/workspace-config.model.ts
+++ src/app/workspace-config/workspace-config.model.ts
@@ -3,4 +3,9 @@ import { Box } from "../box.model";
 export class WorkspaceConfigModel {
     name: String;
     boxes: Array<Box>;
+
+    constructor(name: String) {
+        this.name = name;
+        this.boxes = [];
+    }
 }
