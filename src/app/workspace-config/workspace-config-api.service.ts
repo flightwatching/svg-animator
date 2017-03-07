413e108902907be46b35ad996920d94a57fe9614
kladier <kevin.ladier@gmail.com>
2017-03-07 17:17:12 +0100

    add tests for config management and remove useless getConfig

--- src/app/workspace-config/workspace-config-api.service.ts
+++ src/app/workspace-config/workspace-config-api.service.ts
@@ -26,25 +26,6 @@ export class WorkspaceConfigAPIService {
             });
     }
 
-    getConfig(gridConfigName: String): Observable<any> {
-        return this.http.get(`${environment.config.API_BASE_URL}grid-config/${gridConfigName}`)
-            .map((res: Response) => (res.status === 200 || res.status === 304)?  { status: res.status, config: res.json() } : { status: res.status})
-            .catch((error: Response) => {
-                if (error.status === 400) {
-                    return Observable.throw(new Error(`${error.status} ${error.json().message}`));
-                }
-                else if (error.status === 404) {
-                    return Observable.throw(new Error(`${error.status} Not Found`));
-                }
-                else if (error.status >= 500) {
-                    return Observable.throw(new Error("Server error, try later"));
-                }
-                else {
-                    return Observable.throw(new Error('No response from server'));
-                }
-            });
-    }
-
     saveConfig(config: WorkspaceConfigModel): Observable<Response> {
         return this.http.post(`${environment.config.API_BASE_URL}grid-config/${config.name}`, { config: config })
             .map((res: Response) => (res.status === 200)?  { status: res.status, message: res.json().message } : { status: res.status})
