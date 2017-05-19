ec2daf48c475e408f419c0066fd18082e565be95
Kevin LADIER <kevin.ladier@makina-corpus.com>
2017-05-19 15:24:35 +0200

    Fix collapse items in sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -22,6 +23,7 @@ import { CollapseModule } from 'ng2-collapse';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     MaterialModule,
